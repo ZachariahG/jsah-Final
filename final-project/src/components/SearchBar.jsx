0287a1af29fb84605f60695339d4d4920ccc6403
Nailah Davis <ndavis40@emich.edu>
2025-01-23 21:54:04 -0500

    working on Search Bar

---
+++ final-project/src/components/SearchBar.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import axios from 'axios';
+
+//game title, image, summary, price, avg player count
+
+function SearchBar() {
+    const [query, setQuery] = useState('');
+    const [results, setResults] = useState([]);
+
+    const handleSearch = () => {
+        axios.get(`Steamplace=${query}`)
+        .then((res) => setResults(res.data))
+        .catch((err) => console.error('No results found:', err))
+    };
+ 
+    return (
+        <div>
+        <input 
+        type="text" maxLength={40}
+        placeholder="Search for A Game..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        />
+        <button onClick={handleSearch}>Search</button>
+
+        <ul> 
+            {results.map((game) =>
+            <li key={game.steamId}>{game.name}--{game.description}--{game.image}--{game.price}--{game.players}</li>
+            )}
+        </ul>
+        </div>
+    )
+    
+};
+
+export default SearchBar;
