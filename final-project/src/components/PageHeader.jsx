b9a6aea92c2b2ea8f09138664b2cb3088813bad1
Zachariah Gregg <gregg_z6986@hotmail.com>
2025-01-28 17:20:30 -0500

    Updates

--- final-project/src/components/PageHeader.jsx
+++ final-project/src/components/PageHeader.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import logo from '/src/images/logo.png';
 import './styles/PageHeader.css';
+import SearchBar from "./SearchBar";
 
 function Header () {
     return (
@@ -9,12 +10,12 @@ function Header () {
             <img className="logo" src={logo} alt="Logo" />
             <div className="header-nav">
                 <p className="sort-box">Sort goes here</p>
-                <p className="search-bar">Search bar goes here</p>
+                <SearchBar/>
                 <p className="cart">Cart goes here</p>
             </div>
         </header>
     )
-}
+};
 
 export default Header;
 
