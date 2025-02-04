98e996a73905e31a97539dcee9759cd0ee4286c3
Nailah Davis <ndavis40@emich.edu>
2025-02-04 17:28:18 -0500

    please, my code, it is very sick

--- final-project/src/components/SearchBar.jsx
+++ final-project/src/components/SearchBar.jsx
@@ -1,18 +1,51 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from 'axios';
-import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
+
+const CLIENT_ID = import.meta.env.VITE_REACT_APP_TWITCH_CLIENT_ID;
+const CLIENT_SECRET = import.meta.env.VITE_REACT_APP_TWITCH_API_TOKEN;
 
 
 function SearchBar() {
-    // const { name } = useParams<{name: string}>('');
     const [query, setQuery] = useState('');
     const [results, setResults] = useState([]);
+    const [accessToken, setAccessToken] = useState(null);
+    
+    const games = query;
+    
+    useEffect(() => {
+        const fetchToken = async () => {
+          try {
+            const response = await axios.post("https://id.twitch.tv/oauth2/token", null, {
+              params: {
+                client_id: CLIENT_ID,
+                client_secret: CLIENT_SECRET,
+                grant_type: "client_credentials",
+              },
+            });
+            setAccessToken(response.data.access_token);
+          } catch (error) {
+            console.error("Error fetching access token:", error);
+            setError("Failed to fetch access token.");
+          }
+        };
+    
+        fetchToken();
+      },);
 
     const handleSearch = () => {
         e.preventDefault();
-        axios.get(`https://api.twitch.tv/helix/games?${name}`)
-        .then((res) => setResults(res.data))
+        axios.get(`https://api.twitch.tv/helix/games?name=${query}`,
+            {
+                headers: {
+                  "Client-ID": CLIENT_ID,
+                  Authorization: `Bearer ${accessToken}`,
+                },
+                params: {
+                  first: 1, //get 1 result
+                },})
+
+        .then((res) => setResults(res.data.games))
         .catch((err) => console.error('No results found:', err))
     };
  
@@ -24,11 +57,17 @@ function SearchBar() {
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         />
+        
         <button onClick={handleSearch}>Search</button>
 
         <ul> 
-            {results.map((games) =>
-            <li key={games.id}>{games.name}--{games.box_art_url || "Image Unavailable"}--<Link to={`https://igbd.com/${games.name}`} /> </li>)}
+    
+                    <li key={games.id}>
+                        {games.name}
+                        {games.box_art_url || "Image Unavailable"}
+                    {<Link to={`https://igbd.com/${games.name}`} />}</li>
+        
+            
         </ul>
         </div>
     )
