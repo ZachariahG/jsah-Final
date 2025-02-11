28e516a233bb803a1553e8232d137ad8a410b4a9
Kolstrike <kolstrike1@gmail.com>
2025-02-10 22:03:56 -0500

    idk what I changed

--- final-project/src/components/buttons/Add-To-Cart-Button.jsx
+++ final-project/src/components/buttons/Add-To-Cart-Button.jsx
@@ -10,7 +10,7 @@ const FetchProducts = () => {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await fetch('https://fakestoreapi.com/products?limit=1');
+        const response = await fetch('https://fakestoreapi.com/products?limit=5');
         const data = await response.json();
         setProducts(data);
 
